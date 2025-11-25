3887f3e0cc208f50802f75ec5346022c0c793da9
Caio Durazzo <caio.durazzo220704@gmail.com>
2025-11-25 09:20:23 -0300

    config

--- config.py
+++ config.py
@@ -1,12 +1,1 @@
 data_folder = 'data'
-
-
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-CITY = os.getenv("CITY")
-STATE = os.getenv("STATE")
-COUNTRY_ISO = os.getenv("COUNTRY_ISO")
-AZURE_KEY = os.getenv("AZURE_KEY")
